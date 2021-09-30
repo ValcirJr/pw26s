06e77bbb3db26e9224388e108434bd3885b5c333
valcir <valcir@viasoft.com.br>
2021-09-30 19:55:42 -0300

    Initial commit

---
+++ src/main/java/br/utfpr/edu/pw26s/Pw26sApplication.java
@@ -0,0 +1,13 @@
+package br.utfpr.edu.pw26s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pw26sApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pw26sApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/utfpr/edu/pw26s/Pw26sApplicationTests.java
@@ -0,0 +1,13 @@
+package br.utfpr.edu.pw26s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pw26sApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
